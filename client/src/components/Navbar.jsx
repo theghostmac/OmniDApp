b36bf08908d63087070697d09c93d0c74ce5b789
Ghost Mac <theghostmac@gmail.com>
2022-04-18 22:29:39 -0400

    write tailwind for navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-// import { HiMenuAlt4 } from 'react-icons/hi';
-// import { AiOutlineClose } from 'react-icons/ai';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
@@ -7,7 +7,11 @@ import logo from '../../images/logo.png';
 
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        // w-full will preserve the website to full scale
+        // search for the meaning of each keyword in the tailwindcss documentation
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'> 
+
+        </nav>
     );
 }
 
