744f6cfd2dfe6786244a80e1aa334a22da3ecec2
Ghost Mac <theghostmac@gmail.com>
2022-04-19 13:16:11 -0400

    tranfer event | contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    
+    uint256 transactionCounter;
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+
+
+    struct Transfer {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
 }
