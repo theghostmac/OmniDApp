35d3020894f029e3671c38e233a24daa1260ed58
Ghost Mac <theghostmac@gmail.com>
2022-04-19 10:37:40 -0400

    added a new classProps class for navigation bar | client

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,14 +3,26 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
-
+const NavbarItem = ({ title, classProps }) => {
+    return  (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    );
+}
 
 const Navbar = () => {
     return (
         // w-full will preserve the website to full scale
         // search for the meaning of each keyword in the tailwindcss documentation
         <nav className='w-full flex md:justify-center justify-between items-center p-4'> 
-            <div className='md:flex-[0.5] fllex-i'
+            <div className='md:flex-[0.5] flex-iniitial justify-center items-center'>
+                <img src={logo} alt='logo' className='w-32 cursor-pointer'></img>
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+
+
+            </ul>
         </nav>
     );
 }
