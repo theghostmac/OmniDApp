24d0830762bb366b2d0808d5c7f7179b2a7aa337
Ghost Mac <theghostmac@gmail.com>
2022-04-20 14:20:50 -0400

    added dummyData and shortenAddress

---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        id: 1,
+        url: "https://",
+        message: "",
+        timestamp: "4/20/2022, 7:10 PM",
+        addressFrom: "0xAnything",
+        amount: "0.3",
+        addressTo: "0xAnythingElse"
+    }
+]
